ca679492a79e660703d98332c6f0e71f753eb32a
hienviluong125 <hienviluong125@gmail.com>
2021-02-09 14:59:21 +0700

    blog management API for client

---
+++ controllers/blog.controller.js
@@ -0,0 +1,68 @@
+const { authenticateToken, authorization } = require('./../middlewares/auth.middleware');
+const { Blog } = require('./../models/index');
+const pagy = require('./../utils/pagy');
+const router = require('express').Router();
+
+router.get('/', authenticateToken, authorization(['admin', 'partner', 'member']), async function (req, res) {
+  const page = typeof (req.query.page) !== 'undefined' ? parseInt(req.query.page) : 1;
+  const queryOption = {
+    user_id: req.user.id,
+  }
+  const blogPagy = await pagy({ model: Blog, limit: 10, page: page, queryOption: queryOption });
+
+  res.json({ success: true, blogPagy });
+});
+
+// {
+//   "title": "aaa",
+//   "short_description": "ahihi",
+//   "content": "hello world !!!!!!"
+// }
+router.post('/create', authenticateToken, authorization(['admin', 'partner', 'member']), async function (req, res) {
+  const { title, content, short_description } = req.body;
+
+  const blog = await Blog.build({ title, content, short_description, user_id: req.user.id });
+
+  if (await blog.save()) {
+    return res.json({ success: true, blog: { title, content, short_description } });
+  } else {
+    return res.status(500).json({ success: false });
+  }
+});
+
+router.get('/:id/show', authenticateToken, authorization(['admin', 'partner', 'member']), async function (req, res) {
+  const blog = await Blog.findOne({ where: { id: req.params.id, user_id: req.user.id } });
+
+  res.json({ success: true, blog });
+});
+
+// {
+//   "title": "aaa updated",
+//   "short_description": "ahihi updated",
+//   "content": "hello world !!!!!! updated"
+// }
+router.post('/:id/update', authenticateToken, authorization(['admin', 'partner', 'member']), async function (req, res) {
+  const { title, content, short_description } = req.body;
+
+  let blog = await Blog.findOne({ where: { id: req.params.id, user_id: req.user.id } });
+
+  blog.title = title;
+  blog.content = content;
+  blog.short_description = short_description;
+
+  if (await blog.save()) {
+    return res.json({ success: true, blog });
+  } else {
+    return res.status(500).json({ success: false });
+  }
+});
+
+router.post('/:id/delete', authenticateToken, authorization(['admin', 'partner', 'member']), async function (req, res) {
+  if (await Blog.destroy({ where: { id: req.params.id, user_id: req.user.id } })) {
+    return res.json({ success: true });
+  } else {
+    return res.status(500).json({ success: false });
+  }
+});
+
+module.exports = router;
